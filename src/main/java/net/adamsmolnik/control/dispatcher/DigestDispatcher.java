7b6aee071cdcaf9ed731ac07d27ec5a75a3a7986
smolnik <smolnik@wp.pl>
2014-09-09 14:48:13 +0200

    after major changes

--- src/main/java/net/adamsmolnik/control/dispatcher/DigestDispatcher.java
+++ src/main/java/net/adamsmolnik/control/dispatcher/DigestDispatcher.java
@@ -1,25 +1,23 @@
 package net.adamsmolnik.control.dispatcher;
 
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Response;
-import net.adamsmolnik.control.fallback.ServerInstance;
-import net.adamsmolnik.control.fallback.ServerInstanceBuilder;
-import net.adamsmolnik.control.fallback.SetupParams;
+import net.adamsmolnik.entity.EntityReference;
 import net.adamsmolnik.exceptions.ServiceException;
 import net.adamsmolnik.model.digest.DigestRequest;
 import net.adamsmolnik.model.digest.DigestResponse;
+import net.adamsmolnik.newinstance.ServerInstance;
+import net.adamsmolnik.newinstance.ServerInstanceBuilder;
+import net.adamsmolnik.newinstance.SetupParams;
+import net.adamsmolnik.newinstance.SetupParamsView;
+import net.adamsmolnik.provider.EntityProvider;
+import net.adamsmolnik.sender.Sender;
+import net.adamsmolnik.sender.SendingParams;
 import net.adamsmolnik.setup.ServiceNameResolver;
-import net.adamsmolnik.util.Configuration;
 import net.adamsmolnik.util.LocalServiceUrlCache;
 import net.adamsmolnik.util.Log;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 
 /**
  * @author ASmolnik
@@ -29,7 +27,7 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 public class DigestDispatcher {
 
     @Inject
-    private ServerInstanceBuilder sib;
+    private ServerInstanceBuilder<SetupParamsView, ServerInstance> sib;
 
     @Inject
     private LocalServiceUrlCache cache;
@@ -41,41 +39,40 @@ public class DigestDispatcher {
     private ServiceNameResolver snr;
 
     @Inject
-    private Configuration conf;
+    private EntityProvider entityProvider;
+
+    @Inject
+    private Sender<DigestRequest, DigestResponse> sender;
 
     private final String serviceContext = "/digest-service-no-limit";
 
     private final String servicePath = "/ds/digest";
 
-    private AmazonS3Client s3;
+    private final String serviceFullPath = serviceContext + servicePath;
 
-    private String bucketName;
-
-    @PostConstruct
-    private void init() {
-        bucketName = conf.getGlobalValue("bucketName");
-        s3 = new AmazonS3Client();
-    }
-
+    private final Class<DigestResponse> responseClass = DigestResponse.class;
+    
     public DigestResponse execute(DigestRequest digestRequest) {
-        ObjectMetadata md = s3.getObjectMetadata(bucketName, digestRequest.objectKey);
-        long size = md.getContentLength();
-        String serviceFullPath = serviceContext + servicePath;
-        String serviceUrl = cache.getUrl(serviceFullPath);
-        if (serviceUrl != null) {
-            return send(digestRequest, serviceUrl);
+        Map<String, String> md = entityProvider.getMetadata(new EntityReference(digestRequest.objectKey));
+        long size = Long.valueOf(md.get("contentLength"));
+        String cachedServiceUrl = cache.getUrl(serviceFullPath);
+        if (cachedServiceUrl != null) {
+            return sender.send(cachedServiceUrl, digestRequest, responseClass);
         }
-        String url = "http://digest.adamsmolnik.com" + serviceFullPath;
-        if (size < 1000000) {
-            return send(digestRequest, url);
+        String basicServiceUrl = buildServiceUrl( "digest.adamsmolnik.com");
+        if (size < 50_000_000) {
+            return sender.send(basicServiceUrl, digestRequest, responseClass);
         }
         SetupParams sp = new SetupParams().withLabel("time-limited server instance for " + snr.getServiceName()).withInstanceType("t2.micro")
                 .withImageId("ami-e4ba1d8c").withServiceContext(serviceContext);
         ServerInstance newInstance = sib.build(sp);
-        String newServiceUrl = "http://" + newInstance.getPublicIpAddress() + ":8080" + serviceFullPath;
+        String newServiceUrl = buildServiceUrl(newInstance.getPublicIpAddress() + ":8080");
         final DigestResponse response;
         try {
-            response = trySending(digestRequest, newServiceUrl, 3, 5);
+            response = sender.trySending(newServiceUrl, digestRequest, responseClass, new SendingParams().withNumberOfAttempts(3)
+                    .withAttemptIntervalSecs(5).withLogExceptiomAttemptConsumer((message) -> {
+                        log.err(message);
+                    }));
         } catch (Exception e) {
             log.err(e);
             throw new ServiceException(e);
@@ -84,32 +81,9 @@ public class DigestDispatcher {
         newInstance.scheduleCleanup(10, TimeUnit.MINUTES);
         return response;
     }
-
-    private DigestResponse trySending(DigestRequest digestRequest, String serviceUrl, int numberOfAttempts, int attemptIntervalSecs) throws Exception {
-        int attemptCounter = 0;
-        Exception exception = null;
-        while (attemptCounter < numberOfAttempts) {
-            ++attemptCounter;
-            try {
-                return send(digestRequest, serviceUrl);
-            } catch (Exception ex) {
-                if (attemptCounter == numberOfAttempts) {
-                    throw ex;
-                } else {
-                    log.info("Attempt (" + attemptCounter + ") to send failed for url " + serviceUrl + " and request " + digestRequest
-                            + " with reason: " + ex.getLocalizedMessage());
-                }
-                exception = ex;
-            }
-            TimeUnit.SECONDS.sleep(attemptIntervalSecs);
-        }
-        throw exception;
+    
+    private String buildServiceUrl(String serverAddress) {
+        return "http://" + serverAddress + serviceFullPath;
     }
 
-    private DigestResponse send(DigestRequest digestRequest, String digestServiceUrl) {
-        Client client = ClientBuilder.newClient();
-        Entity<DigestRequest> request = Entity.json(digestRequest);
-        Response response = client.target(digestServiceUrl).request().post(request);
-        return response.readEntity(DigestResponse.class);
-    }
 }
--- src/main/java/net/adamsmolnik/setup/dispatcher/WebSetup.java
+++ src/main/java/net/adamsmolnik/setup/dispatcher/WebSetup.java
@@ -5,8 +5,8 @@ import javax.inject.Inject;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import net.adamsmolnik.boundary.dispatcher.SimpleSqsEndpoint;
 import net.adamsmolnik.control.dispatcher.DigestDispatcher;
+import net.adamsmolnik.endpoint.QueueEndpoint;
 import net.adamsmolnik.model.digest.DigestRequest;
 import net.adamsmolnik.setup.ServiceNameResolver;
 import net.adamsmolnik.util.Configuration;
@@ -26,7 +26,7 @@ public class WebSetup implements ServletContextListener {
     private Configuration conf;
 
     @Inject
-    private SimpleSqsEndpoint endpoint;
+    private QueueEndpoint queueEndpoint;
 
     @Inject
     private DigestDispatcher dc;
@@ -37,14 +37,14 @@ public class WebSetup implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         Map<String, String> confMap = conf.getServiceConfMap(snr.getServiceName());
-        endpoint.handleJson((request) -> {
+        queueEndpoint.handleJson((request) -> {
             return dc.execute(request);
         }, DigestRequest.class, confMap.get("queueIn"), confMap.get("queueOut"));
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        endpoint.shutdown();
+        queueEndpoint.shutdown();
         scheduler.shutdown();
     }
 
